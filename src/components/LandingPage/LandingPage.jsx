f5c445d29b07425067a5324945c15aaefdeec9e4
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-06-15 22:56:17 +0530

    Landing Page responsive

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <div className="landing w-full h-[100vh] flex items-center justify-center ">
-      <div className="w-[8vw] flex flex-col items-center justify-between h-full bg-black p-3 ">
+    <div className="landing w-full h-[100vh] xl:flex items-center justify-center ">
+      <div className="xl:w-[8vw] h-[10vh] flex xl:flex-col items-center justify-between xl:h-full bg-black p-3 ">
         <h1 className="text-white ">LOGO</h1>
-        <h1 className="text-white text-3xl mb-40 w-[20vw] -rotate-90  font-semibold">
+        <h1 className="text-white xl:text-3xl xl:mb-40 md:w-[20vw] w-[30vw] xl:-rotate-90  font-semibold">
           Effortless Dining Reservations
         </h1>
       </div>
-      <div className="w-[95vw] h-full bg-white flex items-center justify-center ">
-        <div className="rightContent w-full h-full flex items-center justify-center gap-5 p-8">
-          <div className="videoContent relative w-1/3 h-[70vh] border shadow-lg">
-            <div className="bg-primary -rotate-90 relative top-20 right-10 text-xl text-white font-semibold p-2 rounded-sm w-fit">
+      <div className="w-[95vw]  h-full bg-white flex items-center justify-center ">
+        <div className="rightContent w-full h-full  xl:flex items-center justify-center gap-5 p-8">
+          <div className="videoContent  relative xl:w-1/3 h-[70vh] border shadow-lg">
+            <div className="bg-primary md:rotate-0 xl:-rotate-90 relative xl:top-20 mr-2 xl:right-10 text-xl text-white font-semibold p-2 rounded-sm w-fit">
               RESERVE NOW
             </div>
-            <div className="video absolute right-5 w-[45vh] h-[60vh] ">
+            <div className="video md:w-[80vw]  xl:w-[40vh] h-full xl:h-[60vh] ">
               <video
-                className="w-full h-full object-cover"
+                className="w-full xl:ml-16 lg:ml-16 h-full  md:h-[60vh] object-cover"
                 src="src/assets/TableTap.mp4"
                 autoPlay
                 loop
@@ -32,17 +32,17 @@ const LandingPage = () => {
               />
             </div>
           </div>
-          <div className="detailsContent w-[55vw] p-10 flex flex-col items-center justify-center gap-10 h-[70vh] border shadow-lg">
+          <div className="detailsContent  xl:w-[55vw] w-fit h-fit p-10 flex flex-col mt-16 items-center justify-center gap-10 xl:h-[70vh] border shadow-xl">
             <h1 className="w-full text-3xl font-semibold text-center">
               TableTap
             </h1>
 
-            <h1 className="text-5xl text-start leading-[1.4] font-bold w-[40vw]">
+            <h1 className="md:text-5xl xl:w-[45vw] lg:w-[60vw] lg:text-center md:text-start text-center text-2xl leading-[1.4] font-bold md:w-[40vw]">
               Dine with Ease: Simple and Fast Table Reservations at Your
               Favorite Restaurant
             </h1>
 
-            <p className="text-md text-tertiary font-semibold">
+            <p className="text-md lg:text-center md:text-center text-tertiary font-semibold">
               Welcome to TableTap, your ultimate solution for effortless dining
               reservations. Our intuitive platform allows you to easily book
               your favorite restaurant tables in just a few clicks. No more long
@@ -52,9 +52,13 @@ const LandingPage = () => {
               the stress of booking, only with TableTap.
             </p>
 
-            <div className="flex items-center gap-5">
-              <button className="btn btn-wide bg-secondary text-primary">Join as Restaurant</button>
-              <button className="btn btn-wide bg-secondary text-primary">Join as User</button>
+            <div className=" flex items-center justify-center gap-5">
+              <button className="btn  xl:btn-wide bg-secondary text-primary">
+                Join as Restaurant
+              </button>
+              <button className="btn xl:btn-wide bg-secondary text-primary">
+                Join as User
+              </button>
             </div>
           </div>
         </div>
