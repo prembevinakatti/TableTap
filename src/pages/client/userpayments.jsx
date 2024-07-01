d31605581de2d3cf7707d8a96157278bcc821634
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-07-01 12:24:30 +0530

    ...

--- src/pages/client/userpayments.jsx
+++ src/pages/client/userpayments.jsx
@@ -46,7 +46,7 @@ function Userpayments() {
   };
 
   return (
-    <div className="p-6 bg-gray-100 relative">
+    <div className="p-6 h-[70vh] overflow-auto bg-gray-100 relative">
       <h1 className="text-2xl font-bold mb-6 text-center text-gray-800">
         Your Payments
       </h1>
@@ -64,25 +64,25 @@ function Userpayments() {
                     onClick={() => navigate(`/resprofilepage/${payment.resid}`)}
                     className="text-blue-500 cursor-pointer"
                   >
-                    {payment.userid}
+                    {payment.resid}
                   </span>
                 </p>
                 <p className="text-lg text-gray-700">
-                  Payment ID: {payment.slug}
+                 <span className="font-semibold"> Payment ID:</span> {payment.slug}
                 </p>
                 <p className="text-lg text-gray-700">
-                  Seat booked day: {JSON.parse(payment.paymentdetails).date}
+                  <span className="font-semibold">Seat booked day:</span> {JSON.parse(payment.paymentdetails).date}
                 </p>
                 <p className="text-lg text-gray-700">
-                  Slot: {JSON.parse(payment.paymentdetails).slot}
+                 <span className="font-semibold"> Slot:</span> {JSON.parse(payment.paymentdetails).slot}
                 </p>
                 <p className="text-lg text-gray-700">
-                  Number of Chairs: {JSON.parse(payment.paymentdetails).numberofchair}
+                  <span className="font-semibold">Number of Chairs:</span> {JSON.parse(payment.paymentdetails).numberofchair}
                 </p>
                 <p className="text-lg text-gray-700">
-                  Reservation ID: {JSON.parse(payment.paymentdetails).reservationid}
+                  <span className="font-semibold">Reservation ID:</span> {JSON.parse(payment.paymentdetails).reservationid}
                 </p>
-                <p className="text-lg text-gray-700">Chair Numbers:</p>
+                <p className="text-lg text-gray-700 font-semibold">Chair Numbers:</p>
                 <div className="flex flex-wrap">
                   {JSON.parse(payment.paymentdetails).chairnumber.map((no) => (
                     <p key={no} className="text-sm text-gray-600 bg-gray-200 px-2 py-1 rounded mr-2 mb-2">
@@ -91,18 +91,20 @@ function Userpayments() {
                   ))}
                 </div>
                 <p className="text-lg text-gray-700">
-                  Payment Status: Successful
+                  <span className="font-semibold">Payment Status:</span> <span className="text-green-500">Successful</span>
                 </p>
               </div>
-              <p className="text-2xl font-semibold text-primary-600">
-                {payment.amount}
+              <div className="w-full flex items-center justify-between">
+              <p className="text-2xl border bg-primary text-tertiary p-2 rounded-md font-semibold text-primary-600">
+                Bill : {payment.amount}
               </p>
               <button
-                className="btn"
+                className="btn btn-wide bg-primary text-tertiary text-2xl p-2 rounded-md"
                 onClick={() => handleShowQr(payment)}
               >
                 Get QR
               </button>
+              </div>
             </div>
           ))}
         </div>
@@ -118,15 +120,16 @@ function Userpayments() {
               </button>
             </form>
             <div style={{ height: "400px", margin: "0 auto", maxWidth: 800, width: "100%" }}>
+            <p className="w-full text-center mb-10 text-2xl text-primary mt-10">
+                Scan QR Code For Details
+              </p>
               <QRCode
                 size={500}
                 style={{ height: "10vw", maxWidth: "100%", width: "100%" }}
                 value={`/paymentdetails/${selectedPayment.slug}`}
                 viewBox={`0 0 256 256`}
               />
-              <p className="w-full text-center text-2xl text-primary mt-10">
-                Scan QR Code For Details
-              </p>
+              
             </div>
           </div>
         </dialog>
