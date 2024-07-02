38572063bf4738adb35cf508a2cb30226e4ab243
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-07-02 09:01:44 +0530

    ..

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,11 +17,17 @@ const Navbar = () => {
   return (
     <div>
       <div className="navbar border-b border-primary px-3 p-2 flex items-center justify-between">
-        <h1>Logo</h1>
+        <div className="w-[5vw] overflow-hidden">
+          <img className="w-full h-full object-cover" src="/public/Logo.png" alt="" />
+        </div>
         <div>
           {profiledata && profiledata.state === "completed" ? (
             <div className="drawer z-30 drawer-end">
-              <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+              <input
+                id="my-drawer-4"
+                type="checkbox"
+                className="drawer-toggle"
+              />
               <div className="drawer-content">
                 <label
                   htmlFor="my-drawer-4"
@@ -36,52 +42,83 @@ const Navbar = () => {
                   aria-label="close sidebar"
                   className="drawer-overlay"
                 ></label>
-               {
-                profiledata&&profiledata.isres===true?( <ul className="menu p-4 w-80 flex flex-col items-center gap-3 min-h-full bg-base-200">
-                  {/* Sidebar content here */}
-                  <div>
-                    <Button details="btn-wide" info="Profile" />
-                  </div>
-                  <div className="flex btn btn-wide bg-secondary text-primary hover:text-primary hover:bg-secondary">
-                    Open
-                    <input type="checkbox" className="toggle toggle-warning z-10" />
-                    Close
-                  </div>
-                  <div>
-                    <Logout />
-                  </div>
-                  <div>
-                    <Button details="btn-wide" info="Bank Details" onClick={() => navigate(`/resbankdetailspage`)} />
-                  </div>
-                  <div>
-                    <Button details="btn-wide" info="all feedbacks" onClick={() => navigate(`/Restorentfeedbackpage`)} />
-                  </div>
-                  <div>
-                    <Button details="btn-wide" info="Qr Scaner" onClick={() => navigate(`/rescodescaner`)} />
-                  </div>
-                  <div>
-                    <Button details="btn-wide" info="Set up cab or bike" onClick={() => navigate(`/restravelsetup`)} />
-                  </div>
-                </ul>):(
-                   <ul className="menu p-4 w-80 flex flex-col items-center gap-3 min-h-full bg-base-200">
-                   {/* Sidebar content here */}
-                   <div>
-                     <Button details="btn-wide" info="Profile"   onClick={() => navigate(`/userprofilepage/${profiledata.$id}`)}/>
-                   </div>
-                  <div>
-                  <Button details="btn-wide" info="Your Reservation"   onClick={() => navigate("/userreservation")}/>
-                  </div>
-                  <div>
-                  <Button details="btn-wide" info="Your feedbacks"   onClick={() => navigate("/Userfeedbackpage")}/>
-                  </div>
-                   <div>
-                     <Logout />
-                   </div>
-                 
-                   
-                 </ul>
-                )
-               }
+                {profiledata && profiledata.isres === true ? (
+                  <ul className="menu p-4 w-80 flex flex-col items-center gap-3 min-h-full bg-base-200">
+                    {/* Sidebar content here */}
+                    <div>
+                      <Button details="btn-wide" info="Profile" />
+                    </div>
+                    <div className="flex btn btn-wide bg-secondary text-primary hover:text-primary hover:bg-secondary">
+                      Open
+                      <input
+                        type="checkbox"
+                        className="toggle toggle-warning z-10"
+                      />
+                      Close
+                    </div>
+                    <div>
+                      <Logout />
+                    </div>
+                    <div>
+                      <Button
+                        details="btn-wide"
+                        info="Bank Details"
+                        onClick={() => navigate(`/resbankdetailspage`)}
+                      />
+                    </div>
+                    <div>
+                      <Button
+                        details="btn-wide"
+                        info="all feedbacks"
+                        onClick={() => navigate(`/Restorentfeedbackpage`)}
+                      />
+                    </div>
+                    <div>
+                      <Button
+                        details="btn-wide"
+                        info="Qr Scaner"
+                        onClick={() => navigate(`/rescodescaner`)}
+                      />
+                    </div>
+                    <div>
+                      <Button
+                        details="btn-wide"
+                        info="Set up cab or bike"
+                        onClick={() => navigate(`/restravelsetup`)}
+                      />
+                    </div>
+                  </ul>
+                ) : (
+                  <ul className="menu p-4 w-80 flex flex-col items-center gap-3 min-h-full bg-base-200">
+                    {/* Sidebar content here */}
+                    <div>
+                      <Button
+                        details="btn-wide"
+                        info="Profile"
+                        onClick={() =>
+                          navigate(`/userprofilepage/${profiledata.$id}`)
+                        }
+                      />
+                    </div>
+                    <div>
+                      <Button
+                        details="btn-wide"
+                        info="Your Reservation"
+                        onClick={() => navigate("/userreservation")}
+                      />
+                    </div>
+                    <div>
+                      <Button
+                        details="btn-wide"
+                        info="Your feedbacks"
+                        onClick={() => navigate("/Userfeedbackpage")}
+                      />
+                    </div>
+                    <div>
+                      <Logout />
+                    </div>
+                  </ul>
+                )}
               </div>
             </div>
           ) : null}
